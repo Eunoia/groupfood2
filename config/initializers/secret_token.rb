f62bc59dc9ab83fa0ee230106abbad0c5d63ec47
Evan R <eunoia.github@gmail.com>
2014-01-07 09:12:58 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupfood2::Application.config.secret_key_base = '3a1091c60324b07349fbdca8746b4628d8f708915cb6044bff56507ad1be5f500b1c3069b13d85f03da5d5da55c1ab7f7d61d3f901310b464f4472ea063afb85'
